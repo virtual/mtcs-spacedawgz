54791c648856d3f739e247f42a1aacf7d0850129
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-21 10:16:35 -0600

    added port listener

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 var express = require('express');
+var app = express(); // invoke express
+
+var port = 5000;
+app.listen(port, function(){
+  // this is called when the app starts LISTENING not when it hears
+  console.log('listening on port', port);
+})
