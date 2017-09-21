15bd36ce9599c90672c4540a3f4d5012983b7707
Jeanine Schoessler <jeanine.mt@gmail.com>
2017-09-21 11:32:48 -0600

    added html paths

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express');
-var app = express(); // invoke express
+var express = require('express'),
+  path = require('path'),
+  app = express(); // invoke express
 
 // statements versus expressions for semi-colons
 
 app.get('/', function(req, res) {
-  res.send("<html><body style='background-image: url(https://i.imgur.com/pnNiUmh.jpg); background-size: cover; background-position: center;}'><div style='background: rgba(255,255,255,.6); padding: 10px; font-size: 2rem; margin: 0 auto; width: 800px; text-align:center;'>get ready <b>for</b> some doggy spacetime<br/><button style='background:#d00; color:#fff; padding: 5px 8px; font-size: 2rem;'>Launch</button></div></body></html>");
+  // res.sendfile("./templates/dawgz.html"); // without path, deprecated
+  res.sendFile(path.join(__dirname + '/templates/index.html'));
 });
 
 app.get('/launch', function(req, res) {
